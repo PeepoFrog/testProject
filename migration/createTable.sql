f6004f2180908ac03c3336a9e35459513f463168
DmitriyBrch <organizm999@gmail.com>
2022-10-29 23:38:48 +0300

    refactoring

---
+++ migration/createTable.sql
@@ -0,0 +1,24 @@
+create table IF NOT EXISTS exampledima(
+TransactionId      int,
+	RequestId          int,
+	TerminalId         int,
+	PartnerObjectId    int,
+	AmountTotal        float8 ,
+	AmountOriginal     float8 ,
+	CommissionPS       float8 ,
+	CommissionClient   float8 ,
+	CommissionProvider float8 ,
+	DateInput          TIMESTAMP,
+	DatePost           TIMESTAMP,
+	Status             VARCHAR,
+	PaymentType        VARCHAR,
+	PaymentNumber      VARCHAR,
+	ServiceId          int,
+	Service            VARCHAR,
+	PayeeId            VARCHAR,
+	PayeeName          VARCHAR,
+	PayeeBankMfo       int,
+	PayeeBankAccount   VARCHAR,
+	PaymentNarrative   VARCHAR
+
+);
